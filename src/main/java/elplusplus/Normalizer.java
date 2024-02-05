2338ec820605a6f9e9e1d4460f4b45f752206479
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2024-02-05 22:39:40 +0100

    added phase two in the normalization

--- src/main/java/elplusplus/Normalizer.java
+++ src/main/java/elplusplus/Normalizer.java
@@ -116,6 +116,85 @@ public class Normalizer
 		}
 	}
 	
+	private void PhaseTwo()
+	{
+		while(!phaseTwoExpressions.isEmpty())
+		{
+			GCI current_gci = phaseOneExpressions.element();
+			phaseTwoExpressions.remove();
+			OWLClassExpression lhs = current_gci.getSubClass();
+			OWLClassExpression rhs = current_gci.getSuperClass();
+			//se lhs e rhs non sono in BC applica NF5
+			if(!Utilities.isInBC(lhs) && !Utilities.isInBC(rhs))
+			{
+				String uniqueID = UUID.randomUUID().toString();
+            	OWLClass newName = dataFactory.getOWLClass(IOR + "#newName" + uniqueID);
+            	phaseTwoExpressions.add(new GCI(lhs, newName));
+            	phaseTwoExpressions.add(new GCI(newName, rhs));
+			}
+			else if(rhs.getClassExpressionType() == ClassExpressionType.OBJECT_SOME_VALUES_FROM)
+			{
+				OWLObjectSomeValuesFrom someValuesFrom = (OWLObjectSomeValuesFrom) rhs;
+				OWLClassExpression filler = someValuesFrom.getFiller();
+				//errore
+				if(Utilities.isInBC(filler) && !Utilities.isInBC(lhs))
+					System.out.println("Found existential restriction on RHS, but the LHS is not in BCD in normalization phase 2. MISTAKE");
+				//forma normale
+				else if(Utilities.isInBC(filler) && Utilities.isInBC(lhs))
+					normalizedExpressions.add(current_gci);
+				//NF6
+				else if(!Utilities.isInBC(filler))
+				{
+					String uniqueID = UUID.randomUUID().toString();
+	            	OWLClass newName = dataFactory.getOWLClass(IOR + "#newName" + uniqueID);
+	            	OWLObjectPropertyExpression property = someValuesFrom.getProperty();
+                	OWLObjectSomeValuesFrom newSomeValuesFrom = dataFactory.getOWLObjectSomeValuesFrom(property, newName);
+                	phaseTwoExpressions.add(new GCI(lhs, newSomeValuesFrom));
+                	phaseTwoExpressions.add(new GCI(newName, filler));
+				}
+			}
+			else if(rhs.getClassExpressionType() == ClassExpressionType.OBJECT_INTERSECTION_OF)
+			{
+				OWLObjectIntersectionOf intersection = (OWLObjectIntersectionOf) rhs;
+                ArrayList<OWLClassExpression> operands = new ArrayList<OWLClassExpression>(intersection.getOperands());
+                OWLClassExpression leftOperand = operands.get(0);
+                OWLClassExpression rightOperand = operands.get(1);
+            	phaseTwoExpressions.add(new GCI(lhs, leftOperand));
+            	phaseTwoExpressions.add(new GCI(lhs, rightOperand));
+			}
+			else
+			{
+				//primo caso: lhs è in BC e rhs è in BC o è il bottom
+				if(Utilities.isInBC(lhs) && Utilities.isInBCOrBottom(rhs))
+					normalizedExpressions.add(current_gci);
+				//secondo caso: lhs è una congiunzione, C1 e C2 devono essere in BC
+				//e D deve essere in BC o bottom
+				else if(lhs.getClassExpressionType() == ClassExpressionType.OBJECT_INTERSECTION_OF && Utilities.isInBCOrBottom(rhs))
+				{
+					OWLObjectIntersectionOf intersection = (OWLObjectIntersectionOf) lhs;
+	                ArrayList<OWLClassExpression> operands = new ArrayList<OWLClassExpression>(intersection.getOperands());
+	                OWLClassExpression leftOperand = operands.get(0);
+	                OWLClassExpression rightOperand = operands.get(1);
+	                if(Utilities.isInBC(leftOperand) && Utilities.isInBC(rightOperand))
+	                	normalizedExpressions.add(current_gci);
+	                else
+	                	System.out.println("Normalization phase two: Found conjunction on LHS, but its parts are not both in the BCD");
+				}
+				//il caso 3 non deve essere controllato: esistenziale su rhs, già controllato prima
+				//caso 4: esistenziale si lhs, il concetto deve essere in BC ed rhs deve essere in BC o bottom
+				else if(lhs.getClassExpressionType() == ClassExpressionType.OBJECT_SOME_VALUES_FROM && Utilities.isInBCOrBottom(rhs))
+				{
+					OWLObjectSomeValuesFrom someValuesFrom = (OWLObjectSomeValuesFrom) lhs;
+					OWLClassExpression filler = someValuesFrom.getFiller();
+					if(Utilities.isInBC(filler))
+						normalizedExpressions.add(current_gci);
+					else
+						System.out.println("Normalization phase two: Found existential on LHS, but its concept is not in BCD: MISTAKE");
+				}
+			}
+		}
+	}
+	
 	private OWLOntology ontology;
 	private Queue<GCI> phaseOneExpressions;
 	private Queue<GCI> phaseTwoExpressions;
