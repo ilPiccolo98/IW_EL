0a6679ccfac5aa65d9dd447a6c36cfbe19c29b06
Giuseppe Piccolo <giuseppe.piccolo@protonmail.com>
2024-02-18 11:23:47 +0100

    removed useless printls

--- src/main/java/elplusplus/ELPlusPlusReasoner.java
+++ src/main/java/elplusplus/ELPlusPlusReasoner.java
@@ -237,8 +237,6 @@ public class ELPlusPlusReasoner {
     }
 
     private boolean isCR6Applied() {
-    	long start = System.currentTimeMillis();
-    	Object mutex = new Object();
     	AtomicBoolean found = new AtomicBoolean(false);
     	oneOfObjects.forEach(nominal -> {
     		mappingS.keySet().forEach(C -> {
@@ -265,9 +263,6 @@ public class ELPlusPlusReasoner {
     			}
     		});
     	});
-    	long finish = System.currentTimeMillis();
-    	long timeElapsed = finish - start;
-    	System.out.println(timeElapsed);
     	return found.get();
     }
 
