6ecea998688ab61b7be98c61edb9ef0d81eb4c69
Francesco Mazza <frances.mazza@studenti.unina.it>
2024-02-22 17:42:02 +0100

    Added methods to get implicit GCIs

--- src/main/java/elplusplus/Utilities.java
+++ src/main/java/elplusplus/Utilities.java
@@ -1,40 +1,105 @@
 package elplusplus;
 
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.Node;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.semanticweb.owlapi.model.AxiomType;
-import org.semanticweb.owlapi.model.ClassExpressionType;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
-import org.semanticweb.owlapi.model.OWLObject;
-import org.semanticweb.owlapi.model.OWLObjectOneOf;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.reasoner.OWLReasoner;
-
-public class Utilities 
+public class Utilities
 {
 	static Set<GCI> getGCIs(OWLOntology ontology, OWLReasoner reasoner)
 	{
 		Set<GCI> classesGCIs = getClassesGCIs(ontology);
+		addSingletonGCIs(ontology, reasoner, classesGCIs);
+		convertPropertyAssertionToGCI(ontology, reasoner, classesGCIs);
 		return classesGCIs;
 	}
-	
+
 	static Set<GCI> getClassesGCIs(OWLOntology ontology)
 	{
 		Set<GCI> gcis = new HashSet<GCI>();
 		Set<OWLSubClassOfAxiom> axioms = ontology.getAxioms(AxiomType.SUBCLASS_OF);
-		for (OWLSubClassOfAxiom axiom : axioms) 
+		for (OWLSubClassOfAxiom axiom : axioms)
 		{
             OWLClassExpression subClass = axiom.getSubClass();
             OWLClassExpression superClass = axiom.getSuperClass();
             gcis.add(new GCI(subClass, superClass));
         }
 		return gcis;
+	}
+
+	private static void addSingletonGCIs(OWLOntology ontology, OWLReasoner reasoner, Set<GCI> GCIs){
+//		Set<OWLClass> classes = ontology.getClassesInSignature();
+//		for (OWLClass cls : classes) {
+//			Set<OWLNamedIndividual> individuals = reasoner.getInstances(cls, true).getFlattened();
+//			for (OWLNamedIndividual individual : individuals) {
+//				OWLObjectOneOf singleton = ontology.getOWLOntologyManager().getOWLDataFactory().getOWLObjectOneOf(individual);
+//				System.out.println("Adding singleton GCI: " + Utilities.prettyPrint(singleton) + " ⊑ " + Utilities.prettyPrint(cls));
+//				GCIs.add(new GCI(singleton, cls));
+//			}
+//		}
+		for (OWLAxiom axiom : ontology.getAxioms()) {
+			// Check if the axiom is an assertion of class membership
+			if (axiom instanceof OWLClassAssertionAxiom) {
+				OWLClassAssertionAxiom classAssertionAxiom = (OWLClassAssertionAxiom) axiom;
+
+				// Retrieve the individual and its class
+				OWLIndividual individual = classAssertionAxiom.getIndividual();
+				OWLClassExpression classExpression = classAssertionAxiom.getClassExpression();
+
+				GCIs.add(new GCI(ontology.getOWLOntologyManager().getOWLDataFactory().getOWLObjectOneOf(individual), classExpression));
+			}
+		}
+	}
+
+	private static void convertPropertyAssertionToGCI(OWLOntology ontology, OWLReasoner reasoner, Set<GCI> GCIs){
+		ontology.getIndividualsInSignature().forEach(individual -> {
+			ontology.getObjectPropertyAssertionAxioms(individual).forEach(axiom -> {
+//				System.out.println(axiom.getSubject() + " " + axiom.getProperty() + " " + axiom.getObject());
+				/*
+				   individual -> property -> object
+				   {individual} ⊑ ∃property.{object}
+				 */
+				OWLObjectPropertyExpression property = axiom.getProperty();
+				OWLObjectOneOf singletonA = ontology
+						.getOWLOntologyManager()
+						.getOWLDataFactory()
+						.getOWLObjectOneOf(individual);
+				OWLObjectOneOf singletonB = ontology
+						.getOWLOntologyManager()
+						.getOWLDataFactory()
+						.getOWLObjectOneOf(axiom.getObject());
+				OWLObjectSomeValuesFrom someValuesFrom = ontology
+						.getOWLOntologyManager()
+						.getOWLDataFactory()
+						.getOWLObjectSomeValuesFrom(property, singletonB);
+				GCIs.add(new GCI(singletonA, someValuesFrom));
+				/*
+				   object -> inverseProperty -> individual
+				   {object} ⊑ ∃inverseProperty.{individual}
+				 */
+				OWLObjectPropertyExpression inverseProperty = findInverseProperty(property, reasoner);
+				if (inverseProperty != null){
+					OWLObjectSomeValuesFrom someValuesFromInverse = ontology
+							.getOWLOntologyManager()
+							.getOWLDataFactory()
+							.getOWLObjectSomeValuesFrom(inverseProperty, singletonA);
+					GCIs.add(new GCI(singletonB, someValuesFromInverse));
+				}
+			});
+		});
+	}
+
+	private static OWLObjectPropertyExpression findInverseProperty(OWLObjectPropertyExpression property, OWLReasoner reasoner){
+		Node<OWLObjectPropertyExpression> inverses = reasoner.getInverseObjectProperties(property);
+		OWLObjectPropertyExpression inverse = (OWLObjectPropertyExpression) inverses.entities().toArray()[0];
+		if (!(inverse instanceof OWLObjectInverseOf)){
+			return inverse;
+		}
+		return null;
 	}
 	
 	static boolean isInBC(OWLObject object)
@@ -113,33 +178,37 @@ public class Utilities
 			return ((OWLNamedIndividual) object).getIRI().getShortForm();
 		else {
 			try {
-				OWLClassExpression expression = (OWLClassExpression) object;
-				switch (expression.getClassExpressionType()) {
-					case OWL_CLASS:
-						return expression.asOWLClass().getIRI().getShortForm();
-					case OBJECT_ONE_OF:
-						return "{"
-								+ ((OWLObjectOneOf) expression).individuals().map(individual -> individual.asOWLNamedIndividual().getIRI().getShortForm())
-										.reduce((s1, s2) -> s1 + ", " + s2).orElse("")
-								+ "}";
-					case OBJECT_INTERSECTION_OF:
-						List<OWLClassExpression> operands = ((OWLObjectIntersectionOf) expression).getOperandsAsList();
-						return "(" + operands.stream().map(Utilities::prettyPrint).reduce((s1, s2) -> s1 + " ⊓ " + s2).orElse("") + ")";
-					case OBJECT_SOME_VALUES_FROM:
-						return "∃" + ((OWLObjectSomeValuesFrom) expression).getProperty().asOWLObjectProperty().getIRI().getShortForm() + "."
-								+ prettyPrint(((OWLObjectSomeValuesFrom) expression).getFiller());
-					case OBJECT_HAS_VALUE:
-						return "∃" + ((OWLObjectHasValue) expression).getProperty().asOWLObjectProperty().getIRI().getShortForm() + ".{"
-								+ prettyPrint(((OWLObjectHasValue) expression).getFiller()) + "}";
-					case OBJECT_COMPLEMENT_OF:
-						return "¬" + prettyPrint(((OWLObjectComplementOf) expression).getOperand());
-					default:
-						throw new RuntimeException("Unknown class expression type " + expression.getClassExpressionType());
+				if (object instanceof OWLClassExpression){
+					OWLClassExpression expression = (OWLClassExpression) object;
+					switch (expression.getClassExpressionType()) {
+						case OWL_CLASS:
+							return expression.asOWLClass().getIRI().getShortForm();
+						case OBJECT_ONE_OF:
+							return "{"
+									+ ((OWLObjectOneOf) expression).individuals().map(individual -> individual.asOWLNamedIndividual().getIRI().getShortForm())
+									.reduce((s1, s2) -> s1 + ", " + s2).orElse("")
+									+ "}";
+						case OBJECT_INTERSECTION_OF:
+							List<OWLClassExpression> operands = ((OWLObjectIntersectionOf) expression).getOperandsAsList();
+							return "(" + operands.stream().map(Utilities::prettyPrint).reduce((s1, s2) -> s1 + " ⊓ " + s2).orElse("") + ")";
+						case OBJECT_SOME_VALUES_FROM:
+							return "∃" + ((OWLObjectSomeValuesFrom) expression).getProperty().asOWLObjectProperty().getIRI().getShortForm() + "."
+									+ prettyPrint(((OWLObjectSomeValuesFrom) expression).getFiller());
+						case OBJECT_HAS_VALUE:
+							return "∃" + ((OWLObjectHasValue) expression).getProperty().asOWLObjectProperty().getIRI().getShortForm() + ".{"
+									+ prettyPrint(((OWLObjectHasValue) expression).getFiller()) + "}";
+						case OBJECT_COMPLEMENT_OF:
+							return "¬" + prettyPrint(((OWLObjectComplementOf) expression).getOperand());
+						default:
+							throw new RuntimeException("Unknown class expression type " + expression.getClassExpressionType());
+					}
+				} else {
+					// it's a role
+					OWLObjectPropertyExpression expression = (OWLObjectPropertyExpression) object;
+					return expression.getNamedProperty().getIRI().getShortForm();
 				}
 			}catch(ClassCastException exception) {
-				// it's a role
-				OWLObjectPropertyExpression expression = (OWLObjectPropertyExpression) object;
-				return expression.getNamedProperty().getIRI().getShortForm();
+				throw exception;
 			}
 		}
 	}
